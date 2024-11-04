9a98737604c5bbf00da4c58e3d9fedfb5f5b86fe
D.Jacklin <Dan@RusticMonkey.com>
2024-11-04 16:15:04 +0000

    RM0003-commit-new-packages

--- EMTool/src/App.tsx
+++ EMTool/src/App.tsx
@@ -1,27 +1,34 @@
 import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, Box } from '@mui/material';
+import HomePage from './HomePage';
+import SettingsPage from './SettingsPage';
 import LoginRegister from './LoginRegister';
 import JiraSettingsMenu from './JiraSettingsMenu';
-import { AppBar, Toolbar, Typography } from '@mui/material';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <AppBar position="fixed">
+    <Router>
+      <AppBar>
         <Toolbar>
           <Typography variant="h6" style={{ flexGrow: 1 }}>
-            EmTool
+            EMTool
           </Typography>
           <LoginRegister />
           <JiraSettingsMenu />
         </Toolbar>
       </AppBar>
-      <Toolbar /> {/* This empty Toolbar pushes the content below the AppBar */}
-      <div style={{ padding: '20px' }}>
-        <h2>Welcome to EmTool!</h2>
-        <p>Your main content goes here.</p>
-      </div>
-    </div>
+
+      <Box component="main" sx={{ p: 3, mt: 2 }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+        </Routes>
+      </Box>
+    </Router>
   );
 };
 
 export default App;
+
+
